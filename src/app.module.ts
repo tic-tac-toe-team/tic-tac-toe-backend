35f85d0932f94e965d06b9780bcd56e46994c89a
anutka-mak <annmakiichuk@gmail.com>
2024-09-09 18:41:07 +0300

    add: controllers, services, repositories, and strategies to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,30 @@
 import { Module } from '@nestjs/common';
+import { PlayerRepository } from './repositories/player.repository';
+import { PlayerService } from './services/player/player.service';
+import { PlayerController } from './controllers/player/player.controller';
+import { PrismaService } from './prisma.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthController } from './controllers/auth/auth.controller';
+import { AuthService } from './services/auth/auth.service';
+import { LocalStrategy } from './services/auth/local.strategy';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+
+const controllers = [
+  PlayerController,
+  AuthController,
+];
+
+const repositories = [
+  PlayerRepository
+];
+
+const services = [
+  PlayerService,
+  AuthService,
+  PrismaService,
+  LocalStrategy,
+  LocalAuthGuard
+];
 
 @Module({
   imports: [
@@ -7,7 +32,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
   ],
-  controllers: [],
-  providers: [],
+  controllers: [...controllers],
+  providers: [...services, ...repositories],
 })
 export class AppModule {}
