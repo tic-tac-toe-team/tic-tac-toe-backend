ed3a6353c00e1395293d57527d25d88c91e022c0
victoriababchenko1 <victoria.babchenko30@gmail.com>
2024-09-09 14:44:12 +0300

    refactored

--- src/services/board/board.service.ts
+++ src/services/board/board.service.ts
@@ -3,31 +3,54 @@ import { BoardRepository } from '../../repositories/board.repository';
 import { SymbolEnum } from '../../enums/symbol.enum';
 import { GameStateEnum } from '../../enums/game-state.enum';
 import { CellService } from '../cell/cell.service';
+import { PlayerGameService } from '../player-game/player-game.service';
+import { Player } from '@prisma/client';
 
 @Injectable()
 export class BoardService {
   constructor(
     private boardRepository: BoardRepository,
     private cellService: CellService,
+    private readonly playerGameService: PlayerGameService,
   ) {}
 
-  async makeMove(gameId: number, position: number, symbol: SymbolEnum): Promise<void> {
+  async createGame(players: Player[]): Promise<number> {
+    if (players.length !== 2) {
+      throw new BadRequestException('A game must have exactly 2 players.');
+    }
+
+    const game = await this.boardRepository.createGame(GameStateEnum.ONGOING);
+
+    await this.addPlayersToGame(game.id, players);
+
+    await this.cellService.createCellsForNewGame(game.id);
+
+    return game.id;
+  }
+
+  private async addPlayersToGame(gameId: number, players: Player[]): Promise<void> {
+
+  }
+
+  async makeMove(gameId: number, position: number): Promise<void> {
     const game = await this.boardRepository.getGameById(gameId);
 
     if (game.state !== GameStateEnum.ONGOING) {
       throw new BadRequestException('Game has already ended');
     }
 
-    try {
-      await this.cellService.fillCell(gameId, position, symbol);
-    } catch (error) {
-      throw new BadRequestException(error);
-    }
+    const currentPlayer = await this.playerGameService.getCurrentPlayer(gameId);
+
+    await this.cellService.fillCell(gameId, position, currentPlayer.symbol as SymbolEnum);
 
     const cells = await this.boardRepository.getCellsByGame(gameId);
     const gameState = this.checkGameState(cells);
 
     await this.boardRepository.updateGameState(gameId, gameState);
+
+    if (gameState === GameStateEnum.ONGOING) {
+      await this.playerGameService.changeCurrentPlayer(gameId);
+    }
   }
 
   private checkGameState(cells: any[]): GameStateEnum {
@@ -61,5 +84,16 @@ export class BoardService {
     }
 
     return GameStateEnum.ONGOING;
+  }
+
+  async getGameBoard(gameId: number): Promise<any> {
+    const game = await this.boardRepository.getGameById(gameId);
+    const cells = await this.boardRepository.getCellsByGame(gameId);
+    return { game, cells };
+  }
+
+  async resetGame(gameId: number): Promise<void> {
+    await this.boardRepository.updateGameState(gameId, GameStateEnum.ONGOING);
+    await this.cellService.resetCells(gameId);
   }
 }
--- src/services/player-game/player-game.service.ts
+++ src/services/player-game/player-game.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
+import { PlayerGame } from '@prisma/client';
+import { PlayerGameRepository } from '../../repositories/player-game.repository';
 
 @Injectable()
-export class PlayerGameService {}
+export class PlayerGameService {
+  constructor(private readonly playerGameRepository: PlayerGameRepository) {}
+
+  async changeCurrentPlayer(gameId: number): Promise<PlayerGame> {
+    const gamePlayers = await this.playerGameRepository.findAllPlayersByGameId(gameId);
+
+    if (gamePlayers.length !== 2) {
+      throw new BadRequestException('Invalid number of players in the game');
+    }
+
+    const currentPlayer = gamePlayers.find((pg) => pg.isCurrentPlayer);
+    const nextPlayer = gamePlayers.find((pg) => !pg.isCurrentPlayer);
+
+    if (!currentPlayer || !nextPlayer) {
+      throw new BadRequestException('Unable to determine current player');
+    }
+
+    await this.playerGameRepository.updatePlayerGame(currentPlayer.id, {
+      isCurrentPlayer: false,
+    });
+
+    return this.playerGameRepository.updatePlayerGame(nextPlayer.id, {
+      isCurrentPlayer: true,
+    });
+  }
+
+  async getCurrentPlayer(gameId: number): Promise<PlayerGame> {
+    const gamePlayers = await this.playerGameRepository.findAllPlayersByGameId(gameId);
+
+    const currentPlayer = gamePlayers.find((pg) => pg.isCurrentPlayer);
+
+    if (!currentPlayer) {
+      throw new NotFoundException('Current player not found for this game');
+    }
+
+    return currentPlayer;
+  }
+}
