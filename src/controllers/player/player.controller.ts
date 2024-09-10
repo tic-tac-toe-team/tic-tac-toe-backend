c5562221663271a2b0b53e3eee95bf18daeea189
anutka-mak <annmakiichuk@gmail.com>
2024-09-10 16:02:34 +0300

    refactor: removed unnecessary code

--- src/controllers/player/player.controller.ts
+++ src/controllers/player/player.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Param, ParseIntPipe, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { PlayerService } from '../../services/player/player.service';
-import { Player } from '@prisma/client';
 import { JwtAuthGuard } from '../../guards/jwt-auth.guard';
 import { PlayerResponseDto } from '../../dtos/player-response.dto';
 
